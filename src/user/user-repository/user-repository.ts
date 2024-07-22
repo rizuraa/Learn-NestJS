bd562332ffb7256a49767617e559a702c4353a1a
Rizuraa <mfmaulana08@gmail.com>
2024-07-22 23:21:03 +0700

    implement winston logger

--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Connection } from '../connection/connection';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
 import { user } from '@prisma/client';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class UserRepository {
-    constructor(private prismaService: PrismaService) {
-        console.info('create user repository')
+    constructor(
+        private prismaService: PrismaService,
+        @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    ) {
+        this.logger.info('create user repository')
     }
 
     async save(firstName: string, lastName?:string): Promise<user>{
+        this.logger.info(
+            `create user with firstname ${firstName} lastname ${lastName}`,
+        );
         return  this.prismaService.user.create({
             data: {
                 first_name: firstName,
